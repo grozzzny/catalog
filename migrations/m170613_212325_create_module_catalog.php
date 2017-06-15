1d6ca44129cba3666c5ecb948925b9670ec27d6e
Dmitriy Groznyy <grozzzny@list.ru>
2017-06-15 08:44:13 +0200

    add module

--- migrations/m170613_212325_create_module_catalog.php
+++ migrations/m170613_212325_create_module_catalog.php
@@ -10,12 +10,18 @@ class m170613_212325_create_module_catalog extends \grozzzny\call_back\migration
             'slug' => $this->string(),
             'title' => $this->string(),
             'parent_id' => $this->integer(),
+            'properties' => $this->text(),
             'image' => $this->string(),
             'views' => $this->integer(),
             'short' => $this->string(),
             'description' => $this->text(),
             'status' => $this->boolean()->defaultValue(1),
             'order_num' => $this->integer()->defaultValue(100),
+        ], $this->tableOptions);
+
+        $this->createTable('gr_catalog_rel', [
+            'category_id' => $this->integer(),
+            'item_id' => $this->integer(),
         ], $this->tableOptions);
 
 
@@ -23,40 +29,33 @@ class m170613_212325_create_module_catalog extends \grozzzny\call_back\migration
             'id' => $this->primaryKey(),
             'slug' => $this->string(),
             'category_id' => $this->integer(),
-            'title' => $this->string(),
-            'parent_id' => $this->integer(),
             'image' => $this->string(),
-            'views' => $this->integer(),
+            'title' => $this->string(),
             'short' => $this->string(),
             'description' => $this->text(),
-            'status' => $this->boolean()->defaultValue(1),
-            'order_num' => $this->integer()->defaultValue(100),
+            'price' => $this->integer(),
+            'discount' => $this->integer(),
+            'views' => $this->integer(),
+            'time_create' => $this->integer(),
+            'time_update' => $this->integer(),
+            'create_at' => $this->integer(),
+            'status' => $this->boolean()->defaultValue(false),
         ], $this->tableOptions);
 
 
         $this->createTable('gr_catalog_data', [
             'id' => $this->primaryKey(),
-            'slug' => $this->string(),
-            'title' => $this->string(),
-            'parent_id' => $this->integer(),
-            'image' => $this->string(),
-            'views' => $this->integer(),
-            'short' => $this->string(),
-            'description' => $this->text(),
-            'status' => $this->boolean()->defaultValue(1),
-            'order_num' => $this->integer()->defaultValue(100),
+            'item_id' => $this->integer(),
+            'property_name' => $this->string(),
+            'value' => 'varchar(1024)',
+            'value_slug' => $this->string(),
         ], $this->tableOptions);
 
 
-        //'create_at' => $this->string(),
-        //'datetime' => $this->integer(),
-        //'datetime_update' => $this->integer(),
-        //price
-
         $this->insert('easyii_modules', [
-            'name' => 'callback',
-            'class' => 'grozzzny\call_back\Module',
-            'title' => 'Call back',
+            'name' => 'Catalog',
+            'class' => 'grozzzny\catalog\CatalogModule',
+            'title' => 'New catalog',
             'icon' => 'font',
             'status' => 1,
             'settings' => '[]',
