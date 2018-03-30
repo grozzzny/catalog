b333b9417574e07c698ac876864069279caad530
grozzzny <1234578>
2018-03-30 10:39:40 +0200

    bug catalog

--- controllers/PropertiesController.php
+++ controllers/PropertiesController.php
@@ -201,13 +201,13 @@ class PropertiesController extends Controller
 
             $fullTitle = Yii::$app->request->get('fullTitle', 'on');
 
-            if(!empty(Yii::$app->request->get('category_id'))){
+            if(!empty($_GET['category_id'])){
                 $query->andWhere(['!=', 'FIND_IN_SET(\''.Yii::$app->request->get('category_id').'\', parents)', '0']);
             }
 
-            if(!empty(Yii::$app->request->get('category_slug'))){
+            if(!empty($_GET['category_slug'])){
                 $identifier = 'slug';
-                $query->andWhere(['!=', 'FIND_IN_SET(\''.$model_category::findOne(['slug' => Yii::$app->request->get('category_slug')])->id.'\', parents)', '0']);
+                $query->andWhere(['!=', 'FIND_IN_SET(\''.Category::findOne(['slug' => Yii::$app->request->get('category_slug')])->id.'\', parents)', '0']);
             }
 
             $data = [];
